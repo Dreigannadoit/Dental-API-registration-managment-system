640f18189f0cc5e487be9cb564258577d7848618
Drei Abmab <dreiabmab@gmail.com>
2025-12-14 14:35:29 +0800

    _

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 
 // Simple CORS for development
 app.use(cors({
-  origin: 'http://localhost:5173' || process.env.CLIENT_URL,
+  origin: process.env.CLIENT_URL || 'http://localhost:5173',
   credentials: true
 }));
 
